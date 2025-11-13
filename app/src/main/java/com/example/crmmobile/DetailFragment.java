90ac41b474c78f77214296e9dc4a23d27fcc0a5b
phong571737 <phong571737@gmail.com>
2025-11-13 23:57:37 +0700

    edit detail lead

--- app/src/main/java/com/example/crmmobile/DetailFragment.java
+++ app/src/main/java/com/example/crmmobile/DetailFragment.java
@@ -44,7 +44,7 @@ public class DetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_detail, container, false);
+        View view =  inflater.inflate(R.layout.fragment_detail_lead, container, false);
         iv_addinforlead = view.findViewById(R.id.iv_addinfor);
         cl_inforlead = view.findViewById(R.id.cl_infor_lead);
 
--- app/src/main/java/com/example/crmmobile/leaddetail.java
+++ app/src/main/java/com/example/crmmobile/leaddetail.java
@@ -34,7 +34,7 @@ public class leaddetail extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_lead, container, false);
+        return inflater.inflate(R.layout.fragment_detail_lead_main, container, false);
 
     }
 
